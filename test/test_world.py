57d04df88cb4680361a19e965c4e98e6dd926cbc
daftspaniel <daftspaniel@gmail.com>
2017-08-22 21:05:38 +0100

    More initial steps?

--- test/test_world.py
+++ test/test_world.py
@@ -8,13 +8,14 @@ class TestWorld(unittest.TestCase):
 
     def test_ctor(self):
         self.world = World()
-        self.assertEqual(self.world.name, 'Default')
+        self.assertEqual(self.world._name, 'Default')
 
     def test_generate(self):
         self.world = World(64, 128)
         self.world.generate()
-        self.assertEqual(self.world.width, 64)
-        self.assertEqual(self.world.height, 128)
+        width, height = self.world.size()
+        self.assertEqual(width, 64)
+        self.assertEqual(height, 128)
 
 
 if __name__ == '__main__':
--- thepass/world.py
+++ thepass/world.py
@@ -1,10 +1,17 @@
 class World(object):
     def __init__(self, width=64, height=64):
-        self.name = 'Default'
+        self._name = 'Default'
         self.width = width
         self.height = height
         self._mapData = None
 
+    @property
+    def name(self):
+        return self._name
+
     def generate(self):
         """ Generate empty map"""
-        self._mapData = [[{}] * self.width for x in range(self.height)]
+        self._mapData = [[{}] * self.height for x in range(self.width)]
+
+    def size(self):
+        return len(self._mapData), len(self._mapData[0])
